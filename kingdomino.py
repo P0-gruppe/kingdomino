10c664a1fdfdea07c76a619046faaa5e184d5a15
baldo <baldo@supernovaa.dev>
2024-09-20 00:01:34 +0200

    first commit

--- kingdomino.py
+++ kingdomino.py
@@ -2,12 +2,13 @@ import cv2 as cv
 import numpy as np
 import os
 
+
 # Main function containing the backbone of the program
 def main():
     print("+-------------------------------+")
     print("| King Domino points calculator |")
     print("+-------------------------------+")
-    image_path = r"C:\Users\admin\Downloads\King Domino dataset\1.jpg"
+    image_path = "./dataset/1.jpg"
     if not os.path.isfile(image_path):
         print("Image not found")
         return
@@ -20,19 +21,23 @@ def main():
             print(get_terrain(tile))
             print("=====")
 
+
 # Break a board into tiles
 def get_tiles(image):
     tiles = []
     for y in range(5):
         tiles.append([])
         for x in range(5):
-            tiles[-1].append(image[y*100:(y+1)*100, x*100:(x+1)*100])
+            tiles[-1].append(image[y * 100 : (y + 1) * 100, x * 100 : (x + 1) * 100])
     return tiles
+
 
 # Determine the type of terrain in a tile
 def get_terrain(tile):
     hsv_tile = cv.cvtColor(tile, cv.COLOR_BGR2HSV)
-    hue, saturation, value = np.mean(hsv_tile, axis=(0,1)) # Consider using median instead of mean
+    hue, saturation, value = np.mean(
+        hsv_tile, axis=(0, 1)
+    )  # Consider using median instead of mean
     print(f"H: {hue}, S: {saturation}, V: {value}")
     if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
         return "Field"
@@ -49,6 +54,7 @@ def get_terrain(tile):
     if 0 < hue < 0 and 0 < saturation < 0 and 0 < value < 0:
         return "Home"
     return "Unknown"
+
 
 if __name__ == "__main__":
     main()
